ac60ea9b09838cd74597a95800d19b742cf28ec5
mikisseth <mikisseth@838ca591-b410-0410-ba70-9e1b5d9a430e>
2006-11-27 21:49:31 +0000

    Finished example

--- dataentity-swing/src/main/java/org/dataentity/example/FlowAgent.java
+++ dataentity-swing/src/main/java/org/dataentity/example/FlowAgent.java
@@ -1,5 +1,7 @@
 package org.dataentity.example;
 
+import javax.swing.SwingUtilities;
+
 import org.dataentity.datamodel.Container;
 import org.dataentity.datamodel.DataProcessor;
 import org.dataentity.datamodel.Valve;
@@ -18,25 +20,33 @@ public class FlowAgent {
 		this.container = container;
 		this.valve = valve;
 	}
-	
+
 	public void start() {
 		thread = new Thread(new Worker());
 		thread.start();
 	}
-	
+
 	public void stop() {
 		thread.interrupt();
 	}
-	
+
 	private class Worker implements Runnable {
 		public void run() {
 			try {
-				if (valve.getOpen()) {
-					Container update = new Container();
-					int currentLevel = container.getWaterVolume();
-					update.setWaterVolume(currentLevel-valve.getFlow());
+				while (true) {
+					if (valve.getOpen()) {
+						final Container update = new Container();
+						int currentLevel = container.getWaterVolume();
+						update.setWaterVolume(currentLevel-valve.getFlow());
+						SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+								System.out.println("Updating: "+update);
+								container.update(update);
+							}							
+						});
+						Thread.sleep(1000);
+					}
 				}
-				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}			
--- dataentity-swing/src/main/java/org/dataentity/numberbean/IntegerFieldBean.java
+++ dataentity-swing/src/main/java/org/dataentity/numberbean/IntegerFieldBean.java
@@ -7,6 +7,7 @@ import javax.swing.JTextField;
 
 import org.dataentity.CompositeView;
 import org.dataentity.DefaultGUIBean;
+import org.dataentity.EventGuard;
 import org.dataentity.TextController;
 import org.dataentity.datamodel.ChangeListener;
 import org.dataentity.datamodel.DataEntity;
@@ -14,49 +15,56 @@ import org.dataentity.datamodel.DataProcessor;
 import org.dataentity.datamodel.ParameterEnum;
 
 public class IntegerFieldBean extends CompositeView {
-    protected final DataEntity model;
-    protected final ParameterEnum parameter;
-    protected final JTextField view;
-    protected TextController controller;
+	protected final DataEntity model;
+	protected final ParameterEnum parameter;
+	protected final JTextField view;
+	protected TextController controller;
+	private final EventGuard guard = new EventGuard();
 
 	protected final DataProcessor processor = new DataProcessor("Integer") {
-        protected void processEntity(DataEntity dataEntity){
-            // Does nothing, acts as root processor
-        }
-    };
-    
-    public IntegerFieldBean(JTextField field,
-                        				   DataEntity globalModel, 
-                        				   ParameterEnum param) {
-        view = field;
-        this.model = globalModel;
-        this.parameter = param;
-        addGUIBean(new DefaultGUIBean(model, 
-            new ChangeListener() {
-            public void handleUpdate(ChangeListener.ChangeEvent dataEntity) {
-                if (dataEntity.getUpdateValues().isDefined(parameter)) {
-                    view.setText(((Integer)dataEntity.getUpdateValues().getAttribute(parameter)).toString()); 
-                }
-            }
-        }));
+		protected void processEntity(DataEntity dataEntity){
+			// Does nothing, acts as root processor
+		}
+	};
 
-        view.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {	
-            DataEntity update = model.createInstance();
-    		update.setAttribute(parameter, new Integer(view.getText()));
-    		processor.process(update);    
-    		model.update(update);
-            }
-        });
-    }
-    public void setEnabled(boolean value) {
-        view.setEnabled(value);
-        view.setFocusable(value);
-    }
-    
-    public JTextField getView() {
-        return view;
-    }
+	public IntegerFieldBean(JTextField field,
+			DataEntity globalModel, 
+			ParameterEnum param) {
+		view = field;
+		this.model = globalModel;
+		this.parameter = param;
+		addGUIBean(new DefaultGUIBean(model, 
+				new ChangeListener() {
+			public void handleUpdate(ChangeListener.ChangeEvent dataEntity) {
+				if (guard. getGuard()) {
+					if (dataEntity.getUpdateValues().isDefined(parameter)) {
+						view.setText(((Integer)dataEntity.getUpdateValues().getAttribute(parameter)).toString()); 
+					}
+					guard.releaseGuard();
+				}
+			}
+		}));  
+
+		view.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {	
+				if (guard. getGuard()) {
+					DataEntity update = model.createInstance();
+					update.setAttribute(parameter, new Integer(view.getText()));
+					processor.process(update);    
+					model.update(update);
+					guard.releaseGuard();
+				}
+			}
+		});
+	}
+	public void setEnabled(boolean value) {
+		view.setEnabled(value);
+		view.setFocusable(value);
+	}
+
+	public JTextField getView() {
+		return view;
+	}
 
 	public void addSubprocessor(DataProcessor subprocesser) {
 		processor.addSubprocessor(subprocesser);
